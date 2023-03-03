275780dc65c27c46aabb000306807448b5c41d4f
Angel <angel-ortega27@hotmail.com>
2023-03-03 10:55:37 -0800

    add computerChoice function to application

--- js/02-rock-paper-scissors.js
+++ js/02-rock-paper-scissors.js
@@ -8,8 +8,20 @@ function getUserChoice() {
     ) {
       alert("Please enter a valid response!");
     } else {
-      console.log(userChoice);
       return userChoice;
     }
   }
 }
+const computerChoice = () => {
+  randomNum = Math.floor(Math.random() * 3) + 1;
+  let cpuChoice;
+  console.log(randomNum);
+  if (randomNum === 1) {
+    cpuChoice = "rock";
+  } else if (randomNum === 2) {
+    cpuChoice = "paper";
+  } else {
+    cpuChoice = "scissors";
+  }
+  return cpuChoice;
+};
