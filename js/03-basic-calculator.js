ead4f45eb3fee37ff8af8e1b5f7861840c9ecba3
Angel <angel-ortega27@hotmail.com>
2023-03-06 17:12:01 -0800

    added calculator module and script files

--- js/03-basic-calculator.js
+++ js/03-basic-calculator.js
@@ -1,8 +1,40 @@
 // IMPORT THE MODULE
+import { add, subtract, multiply, divide } from "./modules/calculator.js";
+let count = 1;
+do {
+  // COLLECT FIRST NUMBER FROM USER
+  let x = parseInt(prompt("Please enter a number"));
+  // COLLECT SECOND NUMBER FROM USER
+  let y = parseInt(prompt("Ehter second number"));
+  // COLLECT OPERATION TO PERFORM (+,-,*,/) FROM USER
 
-// COLLECT FIRST NUMBER FROM USER
-// COLLECT SECOND NUMBER FROM USER
-// COLLECT OPERATION TO PERFORM (+,-,*,/) FROM USER
-
+  let operation = prompt(
+    "Please pick your operation: (add, subtract, multiply, divide)"
+  );
+  if (
+    operation !== "add" &&
+    operation !== "subtract" &&
+    operation !== "multiply" &&
+    operation !== "divide"
+  ) {
+    alert("Please enter a valid operation!");
+  } else {
+    switch (operation) {
+      case "add":
+        add(x, y, "add");
+        break;
+      case "subtract":
+        subtract(x, y, "subtract");
+        break;
+      case "multiply":
+        multiply(x, y, "multiply");
+        break;
+      case "divide":
+        divide(x, y, "divide");
+        break;
+    }
+    count--;
+  }
+} while (count > 0);
 // CHECK TO SEE WHAT OPERATION THEY'RE USING
 // CALL THE APPROPRIATE FUNCTION
--- js/modules/calculator.js
+++ js/modules/calculator.js
@@ -1,13 +1,36 @@
 // ADD A PRIVATE FUNCTION CALLED CALCULATE
-
+function calculate(x, y, func) {
+  if (func === "add") {
+    let z = x + y;
+    return alert(`${x} + ${y} = ${z}`);
+  } else if (func === "subtract") {
+    let z = x - y;
+    return alert(`${x} - ${y} = ${z}`);
+  } else if (func === "multiply") {
+    let z = x * y;
+    return alert(`${x} x ${y} = ${z}`);
+  } else {
+    let z = x / y;
+    return alert(`${x} ÷ ${y} = ${z}`);
+  }
+}
 // ADD FOUR PUBLIC FUNCTIONS BELOW
 //////////////////////////////////
 // ADD FUNCTION
-
+const add = (x, y, operrand) => {
+  calculate(x, y, operrand);
+};
 // SUBTRACT FUNCTION
-
+const subtract = (x, y, opperand) => {
+  calculate(x, y, opperand);
+};
 // MULTIPLY FUNCTION
-
+const multiply = (x, y, opperand) => {
+  calculate(x, y, opperand);
+};
 // DIVIDE FUNCTION
-
+const divide = (x, y, opperand) => {
+  calculate(x, y, opperand);
+};
 // EXPORT THE FOUR PUBLIC FUNCTIONS
+export { add, subtract, multiply, divide };
